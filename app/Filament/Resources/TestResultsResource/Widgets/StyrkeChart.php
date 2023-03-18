68e91dc57405a9f7e745ebcf8af1c5d7e2b03ad3
Tor Rivera <tor@trivera.net>
2023-03-18 10:18:19 +0100

    styretest graf er ferdig

--- app/Filament/Resources/TestResultsResource/Widgets/StyrkeChart.php
+++ app/Filament/Resources/TestResultsResource/Widgets/StyrkeChart.php
@@ -13,30 +13,34 @@ class StyrkeChart extends LineChartWidget
 
     protected function getData(): array
     {
+        //Hente ut data fra DB
         $styrketest = Tests::where('navn', '=', 'Styrketest')->get();
         $resultat = TestResults::where('testsID', '=', $styrketest['0']->id)->orderBy('dato', 'asc')->get();
 
-        $tester = array();
+        //Bygg opp arrayer med dato og resultater
         foreach ($resultat as $r => $v) {
-            foreach ($v->resultat as $k => $res) {
-                foreach ($res as $name => $result) {
-
-                    if (array_search($name, array_column($tester, 'label'))) {
-                        $tester[array_search($name, array_column($tester, 'label'))]['data'][$v->dato->format('d.m.y H:i')] = $result;
-                    } else {
-                        $tester[] = array(
-                            'label' => $name,
-                            'data' => array($v->dato->format('d.m.y H:i') => $result),
-                            'backgroundColor' => 'rgb(' . rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255) . ')'
-                        );
-                    }
-                }
+            $dato[] = $v->dato->format('d.m.y H:i');
+            foreach ($v->resultat[0] as $name => $result) {
+                $resultater[] = array($name => $result);
             }
         }
-        dd($tester); //DumpAndDie
 
+        //Sett sammen arrayene i formatet som trengs for chart.js
+        $finalResults = array();
+        foreach (array_merge_recursive(...$resultater) as $name => $res) {
+            $randColor = 'rgb(' . rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255) . ')';
+            $finalResults[] = array(
+                'label' => $name,
+                'data' => $res,
+                'backgroundColor' => $randColor,
+                'borderColor' => $randColor,
+            );
+        }
+
+        //Voila!
         return [
-            'datasets' => $tester,
+            'datasets' => $finalResults,
+            'labels' => $dato,
         ];
     }
 }
