dc62976a173af9f178109ec4e3e59825e82037fd
Tor Rivera <tor@trivera.net>
2023-03-19 03:49:38 +0100

    lagt til kommentarer og spesifisering av x og y akser

--- app/Filament/Resources/TestResultsResource/Widgets/RheitChart.php
+++ app/Filament/Resources/TestResultsResource/Widgets/RheitChart.php
@@ -11,12 +11,32 @@ class RheitChart extends LineChartWidget
     protected static ?string $heading = 'Rheit test';
     protected static ?string $pollingInterval = null;
 
+    protected static ?array $options = [
+        'scales' => [
+            'x' => [
+                'display' => true,
+                'title' => [
+                    'display' => true,
+                    'text' => 'Dato'
+                ]
+            ],
+            'y' => [
+                'display' => true,
+                'title' => [
+                    'display' => true,
+                    'text' => 'sekunder'
+                ]
+            ]
+        ]
+    ];
+
     protected function getData(): array
     {
-
+        //Hente ut data fra DB
         $rheit = Tests::where('navn', '=', 'Rheit')->get();
         $resultat = TestResults::where('testsID', '=', $rheit['0']->id)->orderBy('dato', 'asc')->get();
 
+        //Bygg opp arrayer med dato og resultater
         foreach ($resultat as $r => $v) {
             $dato[] = $v->dato->format('d.m.y H:i');
             foreach ($v->resultat[0] as $name => $result) {
@@ -24,9 +44,9 @@ class RheitChart extends LineChartWidget
             }
         }
 
+        //Sett sammen arrayene i formatet som trengs for chart.js
         $finalResults = array();
         foreach (array_merge_recursive(...$resultater) as $name => $res) {
-
             $randColor = 'rgb(' . rand(0, 255) . ', ' . rand(0, 255) . ', ' . rand(0, 255) . ')';
             $res = count($dato) > 1 ? $res : [$res];
             $finalResults[] = array(
@@ -36,6 +56,8 @@ class RheitChart extends LineChartWidget
                 'borderColor' => $randColor,
             );
         }
+
+        //Voila!
         return [
             'datasets' => $finalResults,
             'labels' => $dato,
