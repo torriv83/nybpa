e04c5e8b1bff4b5f742c50d7d37e9129db4b431a
Tor Rivera <tor@trivera.net>
2023-04-23 23:06:17 +0200

    Endringer ukeplan

--- resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
+++ resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
@@ -3,20 +3,30 @@
         {{$test['name']}}
     </h2>
 
-    <table class="shadow-lg" style="border-collapse: separate; border-spacing: 5px 1rem;">
+    <table style="border-collapse: separate; border-spacing: 5px 1rem;">
+        @php $antall = 1; @endphp
         @foreach($test['data'] as $d => $e)
             <tr>
                 <th class="bg-gray-500 border text-left px-6 py-4">
                     {{$e['day']}}
                 </th>
+
                 @foreach($e['exercises'] as $data)
                     <td class="border px-6 py-4 " style="background-color: {{$data['intensity']}}">
                         {{$data['exercise']}}
                         <br>
                         {{$data['from']}} - {{$data['to']}}
                     </td>
+                    @if($loop->count < $antall)
+                        <td class="border px-6 py-4 ">&nbsp;</td>
+                    @endif
+
+                    @if($loop->count > $antall)
+                        @php $antall = $loop->count @endphp
+                    @endif
                 @endforeach
             </tr>
+
         @endforeach
     </table>
 
