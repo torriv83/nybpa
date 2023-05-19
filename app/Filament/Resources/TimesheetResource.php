f16f3d423efa2c4677008baf2cec093fdbd0bde7
Tor Rivera <tor@trivera.net>
2023-05-19 13:10:44 +0200

    Endringer i timeliste

--- app/Filament/Resources/TimesheetResource.php
+++ app/Filament/Resources/TimesheetResource.php
@@ -52,13 +52,20 @@ class TimesheetResource extends Resource
 
                 Tables\Columns\TextColumn::make('dato')
                     ->label('Dato')
-                    ->getStateUsing(function (Model $record, $get) {
-                        if ($get('unavailable') == 1) {
+                    ->formatStateUsing(function (string $state, $record) {
+                        if ($record('unavailable') == 1) {
                             return Carbon::parse($record->fra_dato)->format('d.m.Y') . ' - ' . Carbon::parse($record->til_dato)->format('d.m.Y');
                         } else {
                             return Carbon::parse($record->fra_dato)->format('d.m.Y');
                         }
                     })
+                    /*->getStateUsing(function (Model $record, $get) {
+                        if ($get('unavailable') == 1) {
+                            return Carbon::parse($record->fra_dato)->format('d.m.Y') . ' - ' . Carbon::parse($record->til_dato)->format('d.m.Y');
+                        } else {
+                            return Carbon::parse($record->fra_dato)->format('d.m.Y');
+                        }
+                    })*/
                     ->searchable()
                     ->sortable()
                     ->toggleable(),
