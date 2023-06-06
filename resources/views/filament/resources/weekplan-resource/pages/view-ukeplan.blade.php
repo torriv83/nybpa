0fdd5d8c1ab349fa56573dc7aa3644e5e24e7caf
Tor Rivera <tor@trivera.net>
2023-06-06 06:07:41 +0200

    Fikser border på ukeplan tabellen

--- resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
+++ resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
@@ -1,29 +1,44 @@
 <x-filament::page :widget-data="['record' => $record]">
 
     <div class="overflow-y-auto overflow-x-auto">
-        <table class="w-full min-w-full dark:bg-gray-800 text-white border border-collapse dark:border-gray-700">
+        <table class="w-full min-w-full dark:bg-gray-800 text-white dark:border-gray-700 rounded-2xl">
             <thead>
             <tr>
-                <th class="py-2 px-4 border-b border-r dark:border-gray-700">Tid</th>
+                <th class="py-2 px-4 border-b border-r dark:border-gray-700 rounded-l-md w-4">Tid</th>
                 {{-- Empty cell for spacing --}}
                 @foreach($okter as $item)
-                    <th class="py-2 px-4 border-b border-r dark:border-gray-700">{{ $item['day'] }}</th>
+                    @if($item['day'] != 'Søndag')
+                        @php $border = 'border-r';@endphp
+                    @else
+                        @php $border = ''; @endphp
+                    @endif
+                    <th class="py-2 px-4 border-b dark:border-gray-700 {{$border}}">{{ $item['day'] }}</th>
                 @endforeach
             </tr>
             </thead>
             <tbody>
             @foreach($data as $row)
+                @if(!$loop->last)
+                    @php $borderB = 'border-b'; @endphp
+                @else
+                    @php $borderB = ''; @endphp
+                @endif
                 <tr>
                     {{--Display time with x-minute interval--}}
-                    <td class="py-2 px-4 border-b dark:border-gray-600 border-r border-gray-500">
+                    <td class="py-2 px-4 {{$borderB}} dark:border-gray-600 border-r border-gray-500">
                         {{ $row['time'] }}
                     </td>
 
                     @foreach($row['exercises'] as $exercise)
-                        @if (!isset($exercise['intensity']))
-                            <td class="py-2 px-4 border-b dark:border-gray-600 border-r border-gray-500">&nbsp;</td>
+                        @if(!$loop->last)
+                            @php $borderR = 'border-r'; @endphp
                         @else
-                            <td class="py-2 px-4 border-b dark:border-gray-600 border-r border-gray-500"
+                            @php $borderR = ''; @endphp
+                        @endif
+                        @if (!isset($exercise['intensity']))
+                            <td class="py-2 px-4 {{$borderB}} dark:border-gray-600 {{$borderR}} border-gray-500">&nbsp;</td>
+                        @else
+                            <td class="py-2 px-4 {{$borderB}} dark:border-gray-600 {{$borderR}} border-gray-500"
                                 style="{{ getIntensityColorClass($exercise['intensity']) }}">
                                 <div class="mb-1">{{ $exercise['from'] }}</div>
                                 <div>{{ $exercise['exercise'] }}</div>
