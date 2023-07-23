3545166779b4ab97f46f48d59147641ac6fe14b1
Tor Rivera <tor@trivera.net>
2023-07-23 04:54:39 +0200

    clear git cache

--- vendor/phpsa/filament-authentication/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Phpsa\FilamentAuthentication\Actions\ImpersonateLink;
-
-Route::get('/impersonate/stop', fn () => ImpersonateLink::leave())
-    ->name('filament-authentication.stop.impersonation')
-    ->middleware(config('filament-authentication.impersonate.guard'));
--- vendor/saade/filament-fullcalendar/resources/js/filament-fullcalendar.js
+++
@@ -1,5 +0,0 @@
-import calendarComponent from './components/calendar'
-
-document.addEventListener('alpine:init', () => {
-    window.Alpine.plugin(calendarComponent)
-})
