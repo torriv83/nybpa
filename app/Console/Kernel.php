79065ca4c08627062975d4fdc30f994cf464febe
Tor Rivera <tor@trivera.net>
2023-08-21 22:09:34 +0200

    Opprettet automatisk utsending av mail 1 dag, 3 timer og 1 time før en ansatt kommer på jobb.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        Commands\SendWorkReminderEmail::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,6 +20,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('email:send-work-reminder')->everyMinute();
     }
 
     /**
@@ -24,7 +30,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
