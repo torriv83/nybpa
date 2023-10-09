07bec347c83f1ef592bb15038fadb8efd84881fd
Tor Rivera <tor@trivera.net>
2023-10-09 21:45:03 +0200

    Tweaks på widget sessionstats under landslag panel

--- app/Filament/Landslag/Widgets/SessionsStats.php
+++ app/Filament/Landslag/Widgets/SessionsStats.php
@@ -39,36 +39,58 @@ class SessionsStats extends BaseWidget
             }
         }
 
-        $today = Carbon::now()->dayOfWeek + 1; // Convert Sunday to 7, Monday to 1, etc.
+        $nextSession = $this->getNextSession();
 
-        $todayExercises = WeekplanExercise::where('day', $today)
-            ->with(['exercise'])
-            ->get();
+        if ($nextSession['day'] == 0) {
+            $dayString = '';
+        } elseif ($nextSession['day'] == 1) {
+            $dayString = 'i morgen ';
+        } else {
+            $dayString = ucfirst(Carbon::now()->addDays($nextSession['day'])->locale('nb_NO')->isoFormat('dddd')) . ' ';
+        }
 
         return [
             Stat::make('Antall økter', $statistics['okter']),
             Stat::make('Antall timer', CarbonInterval::seconds($statistics['timer'])->cascade()->forHumans()),
             Stat::make('Antall U, V, R økter',
                 'U: ' . $statistics['intensities']['crimson'] . ', V: ' . $statistics['intensities']['darkcyan'] . ', R: ' . $statistics['intensities']['green']),
-            Stat::make('Neste økt', function () use ($todayExercises)
-            {
-                $now = Carbon::now()->format('H:i');
-                $nextSession = null;
-
-                // Sort the exercises by the start_time
-                $todayExercises = $todayExercises->sortBy('start_time');
-
-                // Find the next session based on the current time
-                foreach ($todayExercises as $exercise) {
-                    if ($exercise->start_time > $now) {
-                        $nextSession = "{$exercise->start_time} - {$exercise->end_time}: {$exercise->exercise->name}";
-                        break;
-                    }
-                }
-
-                return $nextSession ?? 'Ingen flere økter i dag';
-            }),
+            Stat::make('Neste økt: ' . $dayString . $nextSession['time'], $nextSession['session']),
         ];
 
     }
+
+    //TODO refactor this
+
+    private function getNextSession(){
+        $now = Carbon::now();
+        $nextSession = [];
+
+        // Iterate through the days of the week starting with today
+        for ($dayOffset = 0; $dayOffset < 7; $dayOffset++) {
+            $day = $now->copy()->addDays($dayOffset)->dayOfWeek;
+            $exercises = WeekplanExercise::where('day', $day)
+                ->with('exercise')
+                ->orderBy('start_time')
+                ->get();
+
+            // If today, filter exercises by start time being later than now
+            if ($dayOffset == 0) {
+                $exercises = $exercises->filter(function($exercise) use ($now) {
+                    return $exercise->start_time > $now->format('H:i');
+                });
+            }
+
+            // If any exercises are found, return the first one
+            if ($exercises->count() > 0) {
+                $exercise = $exercises->first();
+                $nextSession['session'] = "{$exercise->exercise->name}";
+                $nextSession['time'] = Carbon::parse($exercise->start_time)->format('H:i') . '-' . Carbon::parse($exercise->end_time)->format('H:i');
+                $nextSession['day'] = $dayOffset;
+                break;
+            }
+        }
+
+        return $nextSession;
+    }
+
 }
