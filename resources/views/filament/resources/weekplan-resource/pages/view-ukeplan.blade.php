0dc27fd9fc555b1f2b2f7de6f5caea161418962b
Tor Rivera <tor@trivera.net>
2023-10-12 00:23:45 +0200

    feil om at den ikke finner en component. tester..

--- resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
+++ resources/views/filament/resources/weekplan-resource/pages/view-ukeplan.blade.php
@@ -2,18 +2,10 @@
     <div class="overflow-y-auto overflow-x-auto">
         <table class="w-full min-w-full dark:bg-gray-800 text-white dark:border-gray-700 rounded-xl">
 
-{{--            <livewire:landslag.weekplan.table-header/>--}}
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <livewire:Landslag.Weekplan.table-header />
-=======
-            @livewire('landslag.weekplan.table-header')
+{{--            <livewire:Landslag.Weekplan.table-header />--}}
 
->>>>>>> devtest
-=======
 {{--            @livewire('landslag.weekplan.table-header')--}}
 
->>>>>>> devtest
 
             <tbody>
                 <livewire:landslag.weekplan.table-row :weekplanId="$this->weekplan->id"/>
