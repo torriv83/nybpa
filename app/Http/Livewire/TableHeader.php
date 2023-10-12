0d91662006d86112f4652b620c38e7dac37e04b6
Tor Rivera <tor@trivera.net>
2023-10-12 16:15:34 +0200

    feil om at den ikke finner en component. tester..

---
+++ app/Http/Livewire/TableHeader.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Support\Carbon;
+use Livewire\Component;
+
+class TableHeader extends Component
+{
+
+    public $dager = [
+        1 => 'Mandag',
+        2 => 'Tirsdag',
+        3 => 'Onsdag',
+        4 => 'Torsdag',
+        5 => 'Fredag',
+        6 => 'Lørdag',
+        7 => 'Søndag',
+    ];
+
+/*    public function render()
+    {
+        return view('livewire.table-header');
+    }*/
+
+    public function getDayStyles($day): string
+    {
+        $border = $day != 'Søndag' ? 'border-right: 1px solid #52525B;' : '';
+        $today  = $day == ucfirst(Carbon::today()->isoFormat('dddd')) ? 'background-color: grey;' : '';
+        return $border.' '.$today;
+    }
+}
