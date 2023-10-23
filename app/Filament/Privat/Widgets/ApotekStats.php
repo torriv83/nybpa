4cca6c3282dbb78d31fb741ec1def56c98459ea1
Tor Rivera <tor@trivera.net>
2023-10-23 16:32:15 +0200

    Fikset layout på widget.

--- app/Filament/Privat/Widgets/ApotekStats.php
+++ app/Filament/Privat/Widgets/ApotekStats.php
@@ -10,11 +10,13 @@ use Illuminate\Support\Carbon;
 
 class ApotekStats extends BaseWidget
 {
+
+    protected int|string|array $columnSpan = '12';
+
     protected function getStats(): array
     {
-
         $lastOrder = Carbon::parse(\DB::table('filament_email_log')
-            ->where('to', '=','svinesundparken@dittapotek.no')
+            ->where('to', '=', 'svinesundparken@dittapotek.no')
             ->latest()
             ->first()->created_at)->format('d.m.Y H:i');
 
