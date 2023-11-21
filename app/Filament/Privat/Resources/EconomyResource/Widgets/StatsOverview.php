d06d4c182d5e81c90186d84c18ff3230e55aa905
Tor Rivera <tor@trivera.net>
2023-11-21 17:08:46 +0100

    Forbedret koden.

--- app/Filament/Privat/Resources/EconomyResource/Widgets/StatsOverview.php
+++ app/Filament/Privat/Resources/EconomyResource/Widgets/StatsOverview.php
@@ -6,12 +6,13 @@ use App\Models\Economy;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Number;
 
 class StatsOverview extends BaseWidget
 {
-    protected static ?string $pollingInterval = null;
-    protected int|string|array $columnSpan = '12';
-    protected $listeners = ['updateStatsOverview' => '$refresh'];
+    protected static ?string   $pollingInterval = null;
+    protected int|string|array $columnSpan      = '12';
+    protected                  $listeners       = ['updateStatsOverview' => '$refresh'];
 
     protected function getStats(): array
     {
@@ -19,29 +20,96 @@ class StatsOverview extends BaseWidget
 
         $cards = [];
 
-        if ($economy !== null) {
-            $sumBeforeTax = ($economy->before_tax + $economy->grunnstonad) * 12;
-            $sumAfterTax  = ($economy->after_tax + $economy->grunnstonad) * 12;
-            $percent      = (($economy->before_tax - $economy->after_tax) * 100) / $economy->before_tax;
-
-            $ynab     = 'https://api.youneedabudget.com/v1/budgets/d7e4da92-0564-4e8f-87f5-c491ca545435/';
-            $token    = config('app.ynab');
-            $response = Http::withToken($token)->get($ynab . 'months/' . 'current');
-
-            $aom = $response['data']['month']['age_of_money'];
-
-            $cards[] = Stat::make('I året før skatt', number_format($sumBeforeTax, 0, ',', '.') . ' kr')
-                ->description(number_format($sumBeforeTax / 12, 0, ',', '.') . ' kr i måneden');
-            $cards[] = Stat::make('I året etter skatt', number_format($sumAfterTax, 0, ',', '.') . ' kr')
-                ->description(number_format($sumAfterTax / 12, 0, ',', '.') . ' kr i måneden');
-            $cards[] = Stat::make('Prosent skatt', number_format($percent) . '%');
-            $cards[] = Stat::make('Age of money', $aom);
-        } else {
+        if ($economy !== null)
+        {
+            $cards[] = $this->createBeforeTaxCard($economy);
+            $cards[] = $this->createAfterTaxCard($economy);
+            $cards[] = $this->createTaxPercentageCard($economy);
+            $cards[] = $this->createAgeOfMoneyCard();
+        } else
+        {
             // Handle the case when the economy record is not found
             // You can add an appropriate error card or take any other action
             $cards[] = Stat::make('Error', 'Economy record not found');
         }
 
         return $cards;
+    }
+
+    /**
+     * Creates a before-tax card.
+     *
+     * @param  mixed  $economy  The economy object.
+     * @return Stat The created stat object.
+     */
+    private function createBeforeTaxCard(mixed $economy): Stat
+    {
+        $sumBeforeTax = ($economy->before_tax + $economy->grunnstonad) * 12;
+
+        return Stat::make('I året før skatt', self::formatCurrency($sumBeforeTax))
+            ->description(self::formatCurrency($sumBeforeTax, true));
+    }
+
+    /**
+     * Creates an after-tax card.
+     *
+     * @param  mixed  $economy  the economy object
+     * @return Stat the created after-tax card
+     */
+    private function createAfterTaxCard(mixed $economy): Stat
+    {
+        $sumAfterTax = ($economy->after_tax + $economy->grunnstonad) * 12;
+
+        return Stat::make('I året etter skatt', self::formatCurrency($sumAfterTax))
+            ->description(self::formatCurrency($sumAfterTax, true));
+    }
+
+    /**
+     * Calculates the tax percentage from the before-tax and after-tax values.
+     *
+     * @param  mixed  $economy  the object containing the before-tax and after-tax values
+     * @return Stat the created tax percentage card
+     */
+    private function createTaxPercentageCard(mixed $economy): Stat
+    {
+        $percent = (($economy->before_tax - $economy->after_tax) * 100) / $economy->before_tax;
+
+        return Stat::make('Prosent skatt', Number::percentage($percent));
+    }
+
+    /**
+     * Creates an Age of Money card.
+     *
+     * @return Stat The created Age of Money card.
+     */
+    private function createAgeOfMoneyCard(): Stat
+    {
+        $ynab     = 'https://api.youneedabudget.com/v1/budgets/d7e4da92-0564-4e8f-87f5-c491ca545435/';
+        $token    = config('app.ynab');
+        $response = Http::withToken($token)->get($ynab.'months/'.'current');
+
+        $aom = $response['data']['month']['age_of_money'];
+
+        return Stat::make('Age of money', $aom);
+    }
+
+    /**
+     * Format the given amount as a currency string.
+     *
+     * @param  float  $amount  The amount to format.
+     * @param  bool  $month  Whether to format as monthly currency.
+     * @return string The formatted currency string.
+     */
+    public static function formatCurrency(float $amount, bool $month = false): string
+    {
+        $string = ' kr';
+
+        if ($month)
+        {
+            $amount = $amount / 12;
+            $string = ' kr i måneden';
+        }
+
+        return number_format($amount, 0, ',', '.').$string;
     }
 }
