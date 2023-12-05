703b10aefd06da02054c4e02bf470f9c92e90884
Tor Rivera <tor@trivera.net>
2023-12-05 14:44:12 +0100

    Lagt til sortering på 'jobbet i år'

--- app/Filament/Admin/Widgets/Ansatte.php
+++ app/Filament/Admin/Widgets/Ansatte.php
@@ -3,11 +3,13 @@
 namespace App\Filament\Admin\Widgets;
 
 use App\Filament\Admin\Resources\UserResource;
+use App\Models\Timesheet;
 use App\Models\User;
 use Filament\Tables;
 use Filament\Tables\Actions\Action;
 use Filament\Tables\Table;
 use Filament\Widgets\TableWidget as BaseWidget;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 
@@ -59,12 +61,23 @@ class Ansatte extends BaseWidget
 
                         return sprintf('%02d', intdiv($minutes, 60)).':'.(sprintf('%02d', $minutes % 60));
                     })
+                    ->sortable(query: function (Builder $query, string $direction) {
+                        $totalWorkTimeSubquery = Timesheet::selectRaw('SUM(totalt)')
+                            ->whereColumn('user_id', 'users.id')
+                            ->where('unavailable', '!=', 1)
+                            ->whereYear('fra_dato', date('Y'));
+
+                        return $query
+                            ->addSelect(['total_work_time' => $totalWorkTimeSubquery])
+                            ->orderBy('total_work_time', $direction);
+                    })
+
                     ->label('Jobbet i år')
                     ->default('0'),
             ])
             ->paginated([3, 4, 8, 12, 24, 36])
             ->recordUrl(
-                fn(Model $record): string => UserResource::getUrl(),//getUrl('view', ['record' => $record]),
+                fn(Model $record): string => UserResource::getUrl(),
             )
             ->emptyStateHeading('Ingen ansatte er registrert')
             ->emptyStateDescription('Legg til en ansatt for å komme i gang.')
