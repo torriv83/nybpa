8e33588d03e54feab26eb77d49b75835c8e8d125
Tor Rivera <tor@trivera.net>
2024-04-03 08:25:54 +0200

    update

--- resources/views/vendor/pulse/dashboard.blade.php
+++ resources/views/vendor/pulse/dashboard.blade.php
@@ -16,4 +16,8 @@
     <livewire:pulse.slow-jobs cols="6" />
 
     <livewire:pulse.slow-outgoing-requests cols="6" />
+
+    <livewire:pulse.schedule cols="6" />
+
+    <livewire:pulse.about-application />
 </x-pulse>
