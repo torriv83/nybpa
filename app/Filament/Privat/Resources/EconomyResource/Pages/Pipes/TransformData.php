ca6a255446bdf4aeaf1c49b6753368afcdefc360
Tor Rivera <tor@trivera.net>
2024-04-13 20:50:36 +0200

    Add data transformation pipelines for bank transactions
    Added two new classes, TransformData and TransformLines, to handle the processing of a .CSV file containing transactions and the transformation of transactions into a standardized format. Refactored existing code to use these new classes, which simplifies data processing and makes the code more modular and maintainable. Also, adjusted the Makefile update command sequence to a more generic form.

---
+++ app/Filament/Privat/Resources/EconomyResource/Pages/Pipes/TransformData.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by Tor J. Rivera.
+ * Date: 13.04.2024
+ * Time: 14:00
+ * Company: Rivera Consulting
+ */
+
+namespace App\Filament\Privat\Resources\EconomyResource\Pages\Pipes;
+
+use Closure;
+
+class TransformData
+{
+    /**
+     * Handles a .CSV file containing transactions.
+     *
+     * @param string $pathToFile The path to the file containing transactions.
+     * @param \Closure $next The next middleware.
+     *
+     * @return mixed The result of the next middleware.
+     */
+    public function handle($pathToFile, Closure $next)
+    {
+        $getFileContents = file_get_contents($pathToFile);
+        $lines           = explode("\n", preg_replace('/^\xEF\xBB\xBF/', '', $getFileContents));
+        $transactions    = [];
+
+        foreach ($lines as $i => $line) {
+            if ($i == 0 || empty(trim($line))) {
+                continue;
+            }
+
+            if (($data = str_getcsv($line, ';')) && count($data) >= 8) {
+                $transactions[] = $data;
+            }
+        }
+
+        return $next($transactions);
+    }
+}
