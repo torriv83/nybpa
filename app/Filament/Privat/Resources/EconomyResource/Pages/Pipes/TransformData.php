689d1fc5d51d614ab2aeceb0504eb37c2edcb277
Tor Rivera <tor@trivera.net>
2024-04-13 23:27:09 +0200

    Build and tests passed.

--- app/Filament/Privat/Resources/EconomyResource/Pages/Pipes/TransformData.php
+++ app/Filament/Privat/Resources/EconomyResource/Pages/Pipes/TransformData.php
@@ -12,6 +12,8 @@ use Closure;
 
 class TransformData
 {
+    const MINIMUM_DATA_COUNT = 8;
+
     /**
      * Handles a .CSV file containing transactions.
      *
@@ -22,20 +24,34 @@ class TransformData
      */
     public function handle($pathToFile, Closure $next)
     {
-        $getFileContents = file_get_contents($pathToFile);
-        $lines           = explode("\n", preg_replace('/^\xEF\xBB\xBF/', '', $getFileContents));
-        $transactions    = [];
+        $fileContents = $this->readFile($pathToFile);
+        $lines = $this->parseFileContent($fileContents);
+        $transactions = $this->processLines($lines);
+        return $next($transactions);
+    }
 
+    protected function readFile($pathToFile): false|string
+    {
+        return file_get_contents($pathToFile);
+    }
+
+    protected function parseFileContent($fileContents): array
+    {
+        return explode("\n", preg_replace('/^\xEF\xBB\xBF/', '', $fileContents));
+    }
+
+    protected function processLines($lines): array
+    {
+        $transactions = [];
         foreach ($lines as $i => $line) {
             if ($i == 0 || empty(trim($line))) {
                 continue;
             }
 
-            if (($data = str_getcsv($line, ';')) && count($data) >= 8) {
+            if (($data = str_getcsv($line, ';')) && count($data) >= self::MINIMUM_DATA_COUNT) {
                 $transactions[] = $data;
             }
         }
-
-        return $next($transactions);
+        return $transactions;
     }
 }
