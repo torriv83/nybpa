8a4492a9f4775c555dec2436ef07fc66c51012d4
Tor Rivera <tor@trivera.net>
2024-05-01 01:03:22 +0200

    Update email template for prescription expiry notifications
    The email template for expiry notifications of prescriptions has been updated. The listing of expiring and expired prescriptions has been changed from simple paragraphs to ordered lists, improving readability. Moreover, the heading styles have also been modified for better emphasis and clarity.

--- resources/views/emails/prescription_expiry.blade.php
+++ resources/views/emails/prescription_expiry.blade.php
@@ -1,23 +1,25 @@
 <x-mail::message>
-# Resepter går snart ut
-
 @if(!$expiringPrescriptions->isEmpty())
-### Disse reseptene går snart ut:
+# Disse reseptene går snart ut:
 
 <x-mail::panel>
-@foreach($expiringPrescriptions as $d)
-1. {{$d['name']}} går ut: {{ \Carbon\Carbon::parse($d['validTo'])->format('d.m.Y') }}
-@endforeach
+    <ol>
+        @foreach($expiringPrescriptions as $d)
+        <li>{{$d['name']}} går ut: {{ \Carbon\Carbon::parse($d['validTo'])->format('d.m.Y') }}</li>
+        @endforeach
+    </ol>
 </x-mail::panel>
 @endif
 
 @if(!$expiredPrescriptions->isEmpty())
-### Disse reseptene har gått ut:
+# Disse reseptene har gått ut:
 
 <x-mail::panel>
-    @foreach($expiredPrescriptions as $prescription)
-        1. {{ $prescription->name }} gikk ut: {{ \Carbon\Carbon::parse($prescription->validTo)->format('d.m.Y') }}
-    @endforeach
+    <ol>
+        @foreach($expiredPrescriptions as $prescription)
+        <li> {{ $prescription->name }} gikk ut: {{ \Carbon\Carbon::parse($prescription->validTo)->format('d.m.Y') }}</li>
+        @endforeach
+    </ol>
 </x-mail::panel>
 @endif
 
