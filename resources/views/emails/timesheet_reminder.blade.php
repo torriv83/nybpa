9f217d491561b4773e75d323a56858d14e323f41
Tor Rivera <tor@trivera.net>
2024-12-04 16:39:34 +0100

    Update timesheet reminder email subject and content
    Revised the email subject to provide a clearer message and adjusted the email body to present an overview of the hours worked. Removed unnecessary request for a confirmation message from the email content to streamline communication.

--- resources/views/emails/timesheet_reminder.blade.php
+++ resources/views/emails/timesheet_reminder.blade.php
@@ -1,14 +1,11 @@
 <x-mail::message>
-# Det er tid for å sende inn din timeliste!
-Her er tidene som jeg har registrert:
+# Her er en oversikt over timer du har jobbet denne måneden.
 
 <x-mail::panel>
 @foreach($details['timesheets'] as $d)
 1. {{ \Carbon\Carbon::parse($d['fra_dato'])->format('d.m.Y') }}, {{ \Carbon\Carbon::parse($d['fra_dato'])->format('H:i') }} - {{ \Carbon\Carbon::parse($d['til_dato'])->format('H:i') }}
 @endforeach
 </x-mail::panel>
-
-Send meg gjerne en melding når du har sendt inn timelisten, så får jeg godkjent den med en gang!
 
 Mvh.
 Tor J. Rivera
