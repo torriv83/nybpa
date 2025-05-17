f4673bb9faa5c5e4de938ee1d091707006083702
Tor Rivera <tor@trivera.net>
2025-05-17 06:19:56 +0200

    test

--- merge-and-deploy.sh
+++ merge-and-deploy.sh
@@ -5,7 +5,7 @@ CURRENT_BRANCH=$(git symbolic-ref --short -q HEAD)
 
 # Check if on Staging branch, if so exit
 if [ "$CURRENT_BRANCH" = "Staging" ]; then
-  echo "You aree already on the Staging branch. Merge manually if needed."
+  echo "You areee already on the Staging branch. Merge manually if needed."
   exit 1
 fi
 
